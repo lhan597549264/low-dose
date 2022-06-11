b5dfa98ba087f449acb3f90f004119416e6bdaa8
91hanxianshen <597549264@qq.com>
2022-06-11 11:19:30 +0800

    dataset and path

--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 import glob
 import random
 import os
-
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
@@ -26,3 +26,22 @@ class ImageDataset(Dataset):
 
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
+    
+class testDataset(Dataset):
+    def __init__(self,root,transforms_=None):
+        super().__init__() 
+        self.transform = transforms.Compose(transforms_)
+        self.root=root
+        self.dir=os.path.join(self.root,"test","A")
+        self.testlist=os.listdir(self.dir)
+    def __getitem__(self, index):
+        test_dir=os.path.join(self.dir,self.testlist[index])
+        
+        item=self.transform(Image.open(test_dir))
+        return {"name":self.testlist[index],"pic":item}
+    def __len__(self):
+        return len(self.testlist)
+transforms_ = [ transforms.Resize(512),  
+                transforms.ToTensor(),
+                transforms.Normalize((0.5), (0.5)) ]
+
